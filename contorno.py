769fae055a3832434d809ba838a8b3a43d506e01
Negro <fabian_1244@hotmail.com>
2021-07-31 13:10:34 -0400

    funcion de umbral y despliegue

--- contorno.py
+++ contorno.py
@@ -8,13 +8,14 @@ imagen=cv2.imread(path)
 gris=cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
 
 # en este punto se genera un error ya que el threshold genera 2 salidas es por eso 
-#que se debe usar una funcion
-umbral=cv2.threshold(gris,100,255,cv2.THRESH_BINARY)
+#que se debe usar una funcion _, o puede agregarse un segundo parametro para recibir el otro valor (otro, umbral)
+# el otro valor nos devuelve el valor del umbral
+_,umbral=cv2.threshold(gris,100,255,cv2.THRESH_BINARY)
 
 # el codigo a continuacion muestra la imagen
 cv2.imshow('imagen',umbral)
+cv2.imshow('imagen2',gris)
 
-cv2.imshow('imagen',gris)
 #esto evita que se cierre de inmediato
 cv2.waitKey(0)
 cv2.destroyAllWindows()
