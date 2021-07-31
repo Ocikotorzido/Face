79f1a2232789371428c7610ee33d47e613b19c0a
Negro <fabian_1244@hotmail.com>
2021-07-31 13:05:21 -0400

    umbral y threshold

--- contorno.py
+++ contorno.py
@@ -7,9 +7,14 @@ imagen=cv2.imread(path)
 # convertimos la imagen a blanco y negro
 gris=cv2.cvtColor(imagen,cv2.COLOR_BGR2GRAY)
 
-# el codigo a continuacion muestra la imagen
-cv2.imshow('imagen',gris)
+# en este punto se genera un error ya que el threshold genera 2 salidas es por eso 
+#que se debe usar una funcion
+umbral=cv2.threshold(gris,100,255,cv2.THRESH_BINARY)
 
+# el codigo a continuacion muestra la imagen
+cv2.imshow('imagen',umbral)
+
+cv2.imshow('imagen',gris)
 #esto evita que se cierre de inmediato
 cv2.waitKey(0)
 cv2.destroyAllWindows()
