1f7f7abd71525cac3a0e8763b5c3376780ad8f42
Negro <fabian_1244@hotmail.com>
2021-08-03 23:04:03 -0400

    pruebas con reconocimiento

--- reconocimientofacial1/reconocimientofacial.py
+++ reconocimientofacial1/reconocimientofacial.py
@@ -1,23 +1,27 @@
 import cv2 as cv
 import numpy as np
 import os
-import imutils
+
 id=0
 
-modelo='fotos'
+modelo='fotosElon'
 ruta1='C:/Users/nanel/OneDrive/Escritorio/RF/monedascontorno/reconocimientofacial1'
 rutacompleta= ruta1 + '/' + modelo
 if not os.path.exists(rutacompleta):
     os.makedirs(rutacompleta)
 
+
+
+
+
 path = r'C:\Users\nanel\OneDrive\Escritorio\RF\monedascontorno\reconocimientofacial1\haarcascade_frontalface_default.xml'
 ruidos=cv.CascadeClassifier(path)
-camara=cv.VideoCapture(1)
+camara=cv.VideoCapture(r'C:\Users\nanel\OneDrive\Escritorio\RF\monedascontorno\reconocimientofacial1\ElonMusk.mp4')
 
 while True:
     respuesta,captura=camara.read()
     if respuesta==False:break
-    captura=imutils.resize(captura,width=640)
+    
     gris = cv.cvtColor(captura,cv.COLOR_BGR2GRAY)
     
     idcaptura=captura.copy()
@@ -27,7 +31,7 @@ while True:
     for(x,y,e1,e2) in cara:
         cv.rectangle(captura, (x,y), (x+e1,y+e2),(0,255,0),3)
         rostrocapturado=idcaptura[y:y+e1,x:x+e2]
-        rostrocapturado=cv.resize(rostrocapturado,(160,160),interpolacion=cv.INTER_CUBIC)
+        rostrocapturado=cv.resize(rostrocapturado,(160,160),interpolation=cv.INTER_CUBIC)
         cv.imwrite(rutacompleta + '/imagen_{}.jpg'.format(id),rostrocapturado)
         id=id+1
 
