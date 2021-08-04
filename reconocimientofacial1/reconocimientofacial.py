46552a26fee7e73d8bd97acb605db27d917eb189
Negro <fabian_1244@hotmail.com>
2021-08-03 22:38:29 -0400

    captura de rostros

--- reconocimientofacial1/reconocimientofacial.py
+++ reconocimientofacial1/reconocimientofacial.py
@@ -1,3 +1,40 @@
 import cv2 as cv
-ruidos=cv.CascadeClassifier('C:\Users\nanel\OneDrive\Escritorio\RF\opencv\data\haarcascades\haarcascade_frontalface_default.xml')
-print(ruidos)
+import numpy as np
+import os
+import imutils
+id=0
+
+modelo='fotos'
+ruta1='C:/Users/nanel/OneDrive/Escritorio/RF/monedascontorno/reconocimientofacial1'
+rutacompleta= ruta1 + '/' + modelo
+if not os.path.exists(rutacompleta):
+    os.makedirs(rutacompleta)
+
+path = r'C:\Users\nanel\OneDrive\Escritorio\RF\monedascontorno\reconocimientofacial1\haarcascade_frontalface_default.xml'
+ruidos=cv.CascadeClassifier(path)
+camara=cv.VideoCapture(1)
+
+while True:
+    respuesta,captura=camara.read()
+    if respuesta==False:break
+    captura=imutils.resize(captura,width=640)
+    gris = cv.cvtColor(captura,cv.COLOR_BGR2GRAY)
+    
+    idcaptura=captura.copy()
+
+
+    cara=ruidos.detectMultiScale(gris,1.3,5)
+    for(x,y,e1,e2) in cara:
+        cv.rectangle(captura, (x,y), (x+e1,y+e2),(0,255,0),3)
+        rostrocapturado=idcaptura[y:y+e1,x:x+e2]
+        rostrocapturado=cv.resize(rostrocapturado,(160,160),interpolacion=cv.INTER_CUBIC)
+        cv.imwrite(rutacompleta + '/imagen_{}.jpg'.format(id),rostrocapturado)
+        id=id+1
+
+    cv.imshow("Resultado",captura)
+
+    if id==101:
+        break
+
+camara.release()
+cv.destroyAllWindows()
