51832c69edd9456b9ed99d1878e591cf6ee2f610
Negro <fabian_1244@hotmail.com>
2021-08-05 15:36:03 -0400

    entrenamiento de reconocimiento

---
+++ reconocimientofacial1/entrenamiento.py
@@ -0,0 +1,8 @@
+import cv2 as cv
+import os
+import numpy as np
+
+dataRuta = 'C:/Users/nanel/OneDrive/Escritorio/RF/monedascontorno/reconocimientofacial1/Data'
+
+listaData=os.listdir(dataRuta)
+print('data',listaData)
